f5e0335ed606b3b924d1c4cc74af6802e45700c3
Kevin Brown <mrkevinbrown@outlook.com>
2022-11-25 23:21:38 +0000

    New Doctypes

---
+++ placement_file/placement_file/doctype/looked_after_child/looked_after_child.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Kevin Brown and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Looked After Child", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ placement_file/placement_file/doctype/medication_record/medication_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Kevin Brown and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Medication Record", {
+// 	refresh(frm) {
+
+// 	},
+// });
