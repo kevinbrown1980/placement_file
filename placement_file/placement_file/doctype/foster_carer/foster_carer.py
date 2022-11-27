502b5e01bab4727dd3583b96003006ecdd52b04f
Kevin Brown <mrkevinbrown@outlook.com>
2022-11-27 15:47:51 +0000

    Modified DocTypes

--- placement_file/placement_file/doctype/foster_carer/foster_carer.py
+++ placement_file/placement_file/doctype/foster_carer/foster_carer.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Copyright (c) 2022, Kevin Brown and contributors
 # For license information, please see license.txt
 
 # import frappe
+
 from frappe.model.document import Document
 
+
 class FosterCarer(Document):
-	pass
+
+    pass
